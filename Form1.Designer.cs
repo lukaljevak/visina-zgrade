5658daa0876d5533ecb245692a2705c9a8ec0e8b
Luka Ljevak <150526921+lukaljevak@users.noreply.github.com>
2024-03-26 11:59:28 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Visina_zgrade
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtbrojKatova = new System.Windows.Forms.TextBox();
+            this.txtVisinaZgrade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnIzracunaj = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBrojKatova
+            // 
+            this.txtbrojKatova.Location = new System.Drawing.Point(182, 48);
+            this.txtbrojKatova.Name = "txtBrojKatova";
+            this.txtbrojKatova.Size = new System.Drawing.Size(180, 26);
+            this.txtbrojKatova.TabIndex = 0;
+            // 
+            // txtVisinaZgrade
+            // 
+            this.txtVisinaZgrade.Location = new System.Drawing.Point(182, 165);
+            this.txtVisinaZgrade.Name = "txtVisinaZgrade";
+            this.txtVisinaZgrade.Size = new System.Drawing.Size(180, 26);
+            this.txtVisinaZgrade.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Broj katova";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(39, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Visina zgrade";
+            // 
+            // btnIzracunaj
+            // 
+            this.btnIzracunaj.Location = new System.Drawing.Point(43, 80);
+            this.btnIzracunaj.Name = "btnIzracunaj";
+            this.btnIzracunaj.Size = new System.Drawing.Size(310, 79);
+            this.btnIzracunaj.TabIndex = 5;
+            this.btnIzracunaj.Text = "Izračunaj";
+            this.btnIzracunaj.UseVisualStyleBackColor = true;
+            this.btnIzracunaj.Click += new System.EventHandler(this.btnIzracunaj_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnIzracunaj);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtVisinaZgrade);
+            this.Controls.Add(this.txtbrojKatova);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtbrojKatova;
+        private System.Windows.Forms.TextBox txtVisinaZgrade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnIzracunaj;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Visina_zgrade
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIzracunaj_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Neboder neboder = new Neboder();
+                neboder.BrojKatova = Convert.ToInt32(txtbrojKatova.Text);
+                txtVisinaZgrade.Text = neboder.IzracunajVisinu(neboder.BrojKatova).ToString();
+            }
+            catch 
+            {
+                MessageBox.Show("Upisali ste pogrešan format!", "Greška unosa!",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            
+
+        }
+    }
+}
